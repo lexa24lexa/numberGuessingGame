edb0e49e4926ae7f6b399057553831ff4fdfe006
Alexandra <smir0002@hz.nl>
2024-12-22 16:25:36 +0100

    add gameProgress

--- frontend/ObserverPattern.java
+++ frontend/ObserverPattern.java
@@ -37,3 +37,28 @@ class ConsoleObserver implements Observer {
         System.out.println("Notification: " + message);
     }
 }
+
+// GameProgress - Concrete Subject
+class GameProgress extends Subject {
+    private int attemptsLeft;
+
+    public GameProgress(int attempts) {
+        this.attemptsLeft = attempts;
+    }
+
+    public void makeGuess(int guess, int target) {
+        if (guess == target) {
+            notifyObservers("Correct! You guessed the number!");
+        } else {
+            attemptsLeft--;
+            notifyObservers("Wrong guess! Attempts left: " + attemptsLeft);
+            if (attemptsLeft == 0) {
+                notifyObservers("Game Over! The number was: " + target);
+            }
+        }
+    }
+
+    public int getAttemptsLeft() {
+        return attemptsLeft;
+    }
+}
