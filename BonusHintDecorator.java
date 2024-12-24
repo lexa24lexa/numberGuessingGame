0e013d8869727337ed25f17b1cbb5e31a5d9c2e1
Alexandra <smir0002@hz.nl>
2024-12-24 14:45:14 +0100

    add BaseHintDecorator to implement the BonusHint interface

--- BonusHintDecorator.java
+++ BonusHintDecorator.java
@@ -4,3 +4,19 @@ package backend.patterns;
 public interface BonusHint {
     String getHint(int guess, int target);
 }
+
+// Base Hint Decorator implementing BonusHint
+public class BaseHintDecorator implements BonusHint {
+    private BonusHint bonusHint;
+
+    // Constructor
+    public BaseHintDecorator(BonusHint bonusHint) {
+        this.bonusHint = bonusHint;
+    }
+
+    // Pass-through implementation
+    @Override
+    public String getHint(int guess, int target) {
+        return bonusHint.getHint(guess, target);
+    }
+}
