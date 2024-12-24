37f1caa669a4f36e9d255b7af53a3f1ce4abb836
Alexandra <smir0002@hz.nl>
2024-12-24 14:46:51 +0100

    add RangeHintDecorator to provide bonus hints based on proximity

--- BonusHintDecorator.java
+++ BonusHintDecorator.java
@@ -20,3 +20,18 @@ public class BaseHintDecorator implements BonusHint {
         return bonusHint.getHint(guess, target);
     }
 }
+
+// Concrete Decorator for providing bonus hints
+public class RangeHintDecorator extends BaseHintDecorator {
+    public RangeHintDecorator(BonusHint bonusHint) {
+        super(bonusHint);
+    }
+
+    @Override
+    public String getHint(int guess, int target) {
+        String baseHint = super.getHint(guess, target);
+        int range = Math.abs(guess - target);
+        String rangeHint = range > 10 ? "You are far away!" : "You are very close!";
+        return baseHint + " " + rangeHint;
+    }
+}
